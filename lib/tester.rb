b7e607cf951bd24cad7befa4f9eb7ea7bae084a7
sofia alvarez <sofia.alva.gonza@gmail.com>
2022-01-10 05:05:51 -0500

    removed global var in tester (loading in irb)

--- lib/tester.rb
+++ lib/tester.rb
@@ -7,13 +7,13 @@ require_relative 'query'
 
 file_path = 'storage/query-15_4.json'
 
-$profile = Profile.new(file_path)
+profile = Profile.new(file_path)
 
 # controller = Controller.new(profile)
 
 3.times do |i = 4|
   i += 5
-  $profile.update("storage/query-15_#{i}.json")
+  profile.update("storage/query-15_#{i}.json")
 end
 
 pp Post.popular
