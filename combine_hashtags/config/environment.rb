3da0d3620bbfd3ab134195bf428ae774c841d33e
sofia alvarez <sofia.alva.gonza@gmail.com>
2022-01-12 18:05:59 -0500

    added dotenv gem and .env to gitignore

---
+++ combine_hashtags/config/environment.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "../lib/combine_hashtags/post"
+require_relative "../lib/combine_hashtags/profile"
+require_relative "../lib/combine_hashtags/query"
+require_relative "../lib/combine_hashtags/controller"
+require_relative "../lib/combine_hashtags/router"
+require_relative "../lib/combine_hashtags/view"
--- combine_hashtags/lib/combine_hashtags.rb
+++ combine_hashtags/lib/combine_hashtags.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
 
 require_relative "combine_hashtags/version"
+require_relative "../config/environment"
+
+require_relative "../lib/find_recipe/version"
+require_relative "../lib/find_recipe/cli"
+require_relative "../lib/find_recipe/recipe"
+require_relative "../lib/find_recipe/searched_recipe"
+require_relative "../lib/find_recipe/trending_recipe"
+require_relative "../lib/find_recipe/scraper"
 
 module CombineHashtags
   class Error < StandardError; end
 
   # methods for CLI usage
-  class Router
+  class CLI
     def initialize
       @file_path = "storage/query-els00.json"
       @profile = Profile.new(file_path)
@@ -24,7 +32,7 @@ module CombineHashtags
     end
 
     # start cli
-    def start_cli
+    def start
       @router.run
     end
   end
